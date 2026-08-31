dce58a6eb37b1a23ea49c6acc4ba99eb199ca2ba
MOHAMED1234 <mk3426886@gmail.com>
2026-08-31 16:11:28 +0300

    Update index.js

--- backend/server/index.js
+++ backend/server/index.js
@@ -20,7 +20,14 @@ const telegramRoutes = require('../routes/telegram.routes');
 
 const app = express();
 app.set('trust proxy', 1);
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+      "script-src": ["'self'", "'unsafe-inline'"],
+    },
+  },
+}));
 app.use(cors());
 
 app.use(express.json({ limit: '6mb' }));
